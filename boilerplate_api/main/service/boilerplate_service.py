b7823294c0cdffd707a3aff7144dac772b68defd
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2018-11-08 17:34:02 +0000

    works, but would need nginx

--- boilerplate_api/main/service/boilerplate_service.py
+++ boilerplate_api/main/service/boilerplate_service.py
@@ -6,5 +6,5 @@ def boilerplate_service():
     Insert here the business logic of the functions that are called in the "controllers" part of the application
     :return:
     """
-    time.sleep(3)
+    time.sleep(5)
     return "Hi I'm a boilerplate api call"
